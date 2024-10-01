f95be84a92f0409b1f0a4d5262aed90d320bfda6
River <tziony.i@gmail.com>
2024-10-01 13:50:58 +0300

    serperated to parts

--- scripts/args.py
+++ scripts/args.py
@@ -4,7 +4,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description='Process some sequences.')
-    parser.add_argument('--algorithm', type=str, default='FiSSC', help='Algorithm/s to use. Options: paper, greedy, mis_ilp', choices=['FiSSC', 'Greedy', 'MIS_ILP', 'debug_ILP', 'preprocess_test','MIS_ILP_p1','MIS_ILP_p2', 'verify_solution'])
+    parser.add_argument('--algorithm', type=str, default='FiSSC', help='Algorithm/s to use. Options: paper, greedy, mis_ilp', choices=['FiSSC', 'Greedy', 'MIS_ILP', 'debug_ILP', 'preprocess_test','MIS_ILP_p1','MIS_ILP_p2', 'verify_solution', 'p1', 'p2'])
     parser.add_argument('--file_name', type=str, required=True, help='Name of the input file')
     parser.add_argument('--do_logs', action='store_true', help='Enable extensive logging')
     parser.add_argument('--ILP_time_restriction_in_minutes', type=int, default=240, help='ILP time restriction in minutes')
--- tool.py
+++ tool.py
@@ -31,6 +31,38 @@ def test_preprocess(file_path, experiment_name):
     print(f'{experiment_name} done preprocessing in {time.time()-starting_time} seconds')
 
 
+def paper_algorithm_part_1(args, experiment_name, file_path):
+    starting_time = time.time()
+    print(f'Working on {experiment_name}, {file_path}')
+    preprocessed_file = f'data/{experiment_name}.csv'
+    sequences = pd.read_csv(preprocessed_file)['Sequence'].values
+    # create logs folder
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    filter = Filter(sequences, log_file_path=f'logs/{experiment_name}_filter_log.csv', do_logs=args.do_logs)
+    final_working_sequences, final_picked_sequences, history = filter.do_filter()
+    working_sequences_file_path = f'data/{experiment_name}_filtered_working.csv'
+    picked_sequences_file_path = f'data/{experiment_name}_filtered_picked.csv'
+    filter.save_working_csv(working_sequences_file_path)
+    filter.save_picked_csv(picked_sequences_file_path)
+    print(f'{experiment_name} done filtering in {time.time()-starting_time} seconds')
+
+def paper_algorithm_part_2(args, experiment_name, file_path):
+    print(f'Working on {experiment_name}, {file_path}')
+    preprocessed_file = f'data/{experiment_name}.csv'
+    working_sequences_file_path = f'data/{experiment_name}_filtered_working.csv'
+    picked_sequences_file_path = f'data/{experiment_name}_filtered_picked.csv'
+
+
+    starting_time = time.time()
+    ilp_solution(working_sequences_file_path, picked_sequences_file_path, args.ILP_time_restriction_in_minutes, experiment_name, args)
+    print(f'{experiment_name} done ILP in {time.time()-starting_time} seconds')
+
+    validate_solution(preprocessed_file, f'output/{experiment_name}_picked_sequences.csv')
+
+
+
+
 def paper_algorithm(args, experiment_name, file_path):
     starting_time = time.time()
     print(f'Working on {experiment_name}, {file_path}')
@@ -114,6 +146,10 @@ def main():
         mis_ilp_algorithm_p2(args, experiment_name, file_path)
     elif args.algorithm == 'verify_solution':
         validate_fissc_solution(experiment_name)
+    elif args.algorithm == 'p1':
+        paper_algorithm_part_1(args, experiment_name, file_path)
+    elif args.algorithm == 'p2':
+        paper_algorithm_part_2(args, experiment_name, file_path)
 
     else:
         print('Invalid algorithm')
